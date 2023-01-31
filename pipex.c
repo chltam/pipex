ea11276b44b26348daa4eb52d9973abd80402644
chltam <114371464+chltam@users.noreply.github.com>
2023-01-31 19:22:30 +0100

    single quote nightmare

--- pipex.c
+++ pipex.c
@@ -9,30 +9,20 @@
 /*   Updated: 2023/01/24 11:23:20 by htam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include "./libft/libft.h"
+
 #include "pipex.h"
 
 void	ft_error(char *str)
 {
-	ft_printf("%s\n", str);
+	ft_putendl_fd(str, 2);
 	exit(EXIT_FAILURE);
 }
 
-char	*get_path(char *cmd, char *envp[])
+char	*valid_path(char **allpath, char *cmd)
 {
-	char	**allpath;
 	char	*tempcmd;
 	char	*path;
 
-	while (ft_strncmp(*envp, "PATH=", 5) != 0)
-		*envp++;
-	*envp += 5;
-	allpath = ft_split(*envp, ':');
 	tempcmd = ft_strjoin("/", cmd);
 	while (*allpath)
 	{
@@ -49,7 +39,30 @@ char	*get_path(char *cmd, char *envp[])
 		}
 	}
 	free(tempcmd);
-	ft_error("No such command");
+	return (NULL);
+}
+
+char	*get_path(char *cmd, char *envp[])
+{
+	char	**allpath;
+	char	*path;
+
+	while (ft_strncmp(*envp, "PATH=", 5) != 0)
+		*envp++;
+	*envp += 5;
+	allpath = ft_split(*envp, ':');
+	path = valid_path(allpath, cmd);
+	if (path)
+	{
+		free(allpath);
+		return (path);
+	}
+	else
+	{
+		free(allpath);
+		ft_error("No such command");
+		return (NULL);
+	}
 }
 
 void	childlabour(int *fd, char **argv, char **envp)
@@ -70,13 +83,28 @@ void	childlabour(int *fd, char **argv, char **envp)
 	ft_error("ERROR! child survived");
 }
 
+void	parentlabour(int *fd, char **argv, char **envp)
+{
+	int		outfile_fd;
+	char	**cmd;
+
+	outfile_fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (outfile_fd == -1)
+		ft_error("open outfile failed");
+	cmd = ft_split(argv[3], ' ');
+	if (dup2(outfile_fd, STDOUT_FILENO) == -1)
+		ft_error("dup2 failed");
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		ft_error("dup2 failed");
+	close(fd[1]);
+	execve(get_path(cmd[0], envp), cmd, NULL);
+	ft_error("ERROR");
+}
+
 void	pipex(char **argv, char **envp)
 {
 	int		fd[2];
-	int		outfile_fd;
-	char	**cmd;
 	int		child_id;
-	int		infile_fd;
 
 	if (pipe(fd) == -1)
 		ft_error("pipe failed");
@@ -84,31 +112,12 @@ void	pipex(char **argv, char **envp)
 	if (child_id == -1)
 		ft_error("fork failed");
 	if (child_id == 0)
-	{
-		// infile_fd = open(argv[1], O_RDONLY);
-		// cmd = ft_split(argv[2], ' ');
-		// dup2(infile_fd, STDIN_FILENO);
-		// dup2(fd[1], STDOUT_FILENO);
-		// close(fd[0]);
-		// execve(get_path(cmd[0], envp), cmd, NULL);
 		childlabour(fd, argv, envp);
-	}
 	else
 	{
 		waitpid(child_id, NULL, 0);
-		outfile_fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0777);
-		if (outfile_fd == -1)
-			ft_error("open outfile failed");
-		cmd = ft_split(argv[3], ' ');
-		if (dup2(outfile_fd, STDOUT_FILENO) == -1)
-			ft_error("dup2 failed");
-		if (dup2(fd[0], STDIN_FILENO) == -1)
-			ft_error("dup2 failed");
-		close(fd[1]);
-		execve(get_path(cmd[0], envp), cmd, NULL);
-		ft_error("ERROR");
-	}	
-	//ft_error("ERROR");
+		parentlabour(fd, argv, envp);
+	}
 }
 
 int	main(int argc, char *argv[], char *envp[])
